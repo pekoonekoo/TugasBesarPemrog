a99176f68fedde1ec767d71579094353837e7e30
arzario <136824245+arzario@users.noreply.github.com>
2023-07-04 13:09:35 +0700

    Update work4.py

--- work4.py
+++ work4.py
@@ -6,7 +6,7 @@ dc,kantinneo01,makanan,nasi pecel,10000
 dc,kantinneo01,makanan,nasi magelangan,10000
 dc,kantinneo01,minumam,esteh,3000
 dc,kantinneo01,minumam,pop ice,4000
-dc,kantinneo01,minumam,beng benh drink,5000
+dc,kantinneo01,minumam,beng beng drink,5000
 dc,kantinneo01,minumam,good day,4000
 dc,kantinneo01,minumam,kopi hitam,3000
 
