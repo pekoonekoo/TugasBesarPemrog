ab32ad646c0bf57714b1a5b7d9eeb1c010307419
adhithyaNurF <77089373+adhithyaNurF@users.noreply.github.com>
2023-07-04 13:22:26 +0700

    Update work3.py

--- work3.py
+++ work3.py
@@ -1,1 +1,26 @@
-#WorkSpace Adhit
+#WorkSpace adit
+
+#Dc,nama kantin,menu makan/minum,harga
+DC,NEO,Makanan,Nasi Ayam Goreng,10000
+DC,NEO,Makanan,Nasi Ayam Penyet,10000
+DC,NEO,Makanan,Nasi Uduk,10000
+DC,NEO,Makanan,Indomie Ori kuah/goreng,6000
+DC,NEO,Makanan,Indomie telor kuah/goreng,10000
+DC,NEO,Makanan,Nasi Rames,5000
+DC,NEO,Minuman,Es teh,3000
+DC,NEO,Minuman,Air Mineral,4000
+DC,NEO,Minuman,Es Milo,4000
+DC,NEO,Minuman,Es Susu Coklat,4000
+DC,NEO,Minuman,Es Susu Putih,4000
+
+#Kantin TT
+TT,Angkringan Kampus,Makanan,Nasi Teri,3000
+TT,Angkringan Kampus,Makanan,Nasi Suwir Ayam,9000
+TT,Angkringan Kampus,Makanan,Sate Usus,2500
+TT,Angkringan Kampus,Makanan,Sate Kulit,2500
+TT,Angkringan Kampus,Makanan,Indomie Polos,8000
+TT,Angkringan Kampus,Makana,Indomie Telor,9000
+TT,Angkringan Kampus,Minuman,Es Cocolatos,4000
+TT,Angkringan Kampus,Minuman,Es Nutrisari,4000
+TT,Angkringan Kampus,Minuman,Energen,4000
+TT,Angkringan Kampus,Minuman, Es Beng Beng Drink,4000
