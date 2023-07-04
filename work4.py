763e0087b366b68efaf302fde28fb4f805755e50
arzario <136824245+arzario@users.noreply.github.com>
2023-07-04 13:07:58 +0700

    Update work4.py

--- work4.py
+++ work4.py
@@ -9,3 +9,14 @@ dc,kantinneo01,minumam,pop ice,4000
 dc,kantinneo01,minumam,beng benh drink,5000
 dc,kantinneo01,minumam,good day,4000
 dc,kantinneo01,minumam,kopi hitam,3000
+
+tt,kantinbakso&mieayamkampus,makanan,bakso biasa,10000
+tt,kantinbakso&mieayamkampus,makanan,bakso telur,15000
+tt,kantinbakso&mieayamkampus,makanan,bakso urat,15000
+tt,kantinbakso&mieayamkampus,makanan,mie ayam biasa,10000
+tt,kantinbakso&mieayamkampus,makanan,mie ayam bakso,15000
+tt,kantinbakso&mieayamkampus,minuman,teh,3000
+tt,kantinbakso&mieayamkampus,minuman,jeruk,4000
+tt,kantinbakso&mieayamkampus,minuman,kopi,3000
+tt,kantinbakso&mieayamkampus,minuman,susu,4000
+tt,kantinbakso&mieayamkampus,minuman,nutrisari,3000
