d8347451fc040424c3407b93b60ea650e36c943e
arzario <136824245+arzario@users.noreply.github.com>
2023-07-04 13:00:43 +0700

    Update work4.py

--- work4.py
+++ work4.py
@@ -1,1 +1,11 @@
 #WorkSpace Rio
+dc,kantinneo01,makanan,nasi goreng,10000
+dc,kantinneo01,makanan,nasi ayam geprek,12000
+dc,kantinneo01,makanan,nasi rames,7000
+dc,kantinneo01,makanan,nasi pecel,10000
+dc,kantinneo01,makanan,nasi magelangan,10000
+dc,kantinneo01,minumam,esteh,3000
+dc,kantinneo01,minumam,pop ice,4000
+dc,kantinneo01,minumam,beng benh drink,5000
+dc,kantinneo01,minumam,good day,4000
+dc,kantinneo01,minumam,kopi hitam,3000
